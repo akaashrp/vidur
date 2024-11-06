53912a2a2099b6058ea34766472b6b9c489592ac
akaashrp <43900735+akaashrp@users.noreply.github.com>
2024-11-06 01:20:12 -0500

    update dataframe saving

--- vidur/profiling/bandwidth/main.py
+++ vidur/profiling/bandwidth/main.py
@@ -104,13 +104,15 @@ def profile_model(
     
     # Convert results to DataFrame
     df = pd.DataFrame(all_results)
+    print(df.head())
+
     df = (
         pd.json_normalize(df["time_stats"])
         .add_prefix("time_stats.")
         .join(df.drop(columns=["time_stats"]))
     )
 
-    print(df)
+    print(df.head())
     
     # Calculate bandwidth in GB/s
     df["bandwidth_gbps"] = (
